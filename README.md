bbf9506622e3be20d4fa4279bb45e5a36a1827dd
Jonas Bandi <dev@jonasbandi.net>
2019-01-01 02:10:17 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-It's me, Tierney!
+My npm card!
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx jbandi
 ```
-
-Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- build.js
+++ build.js
@@ -15,48 +15,59 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('              Jonas Bandi'),
+  handle: chalk.white('jbandi'),
+  work: chalk.white('Enthusiastic Software Professional'),
+  location: chalk.white('Bern, Switzerland'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('jonasbandi'),
+  xing: chalk.gray('https://www.xing.com/profile/') + chalk.blue('Jonas_Bandi'),
+  cv: chalk.gray('https://cv.jonasbandi.net'),
+  web: chalk.cyan('https://jonasbandi.net'),
+  medium: chalk.gray('https://medium.jonasbandi.net/'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('jbandi'),
+  github: chalk.gray('https://github.com/') + chalk.green('jbandi'),
+  npx: chalk.red('npx') + ' ' + chalk.white('jbandi'),
   labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLocation: chalk.white.bold('   Location:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelXing: chalk.white.bold('       Xing:'),
+  labelCv: chalk.white.bold('         CV:'),
+  Speaking: chalk.white.bold('        Speaking:'),
   labelWeb: chalk.white.bold('        Web:'),
+  labelMedium: chalk.white.bold('     Medium:'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
   labelCard: chalk.white.bold('       Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const work = `${data.labelWork}  ${data.work}`
+const location = `${data.labelLocation}  ${data.location}`
+const linkedin = `${data.labelLinkedIn}  ${data.linkedin}`
+const xing = `${data.labelXing}  ${data.xing}`
+const cv = `${data.labelCv}  ${data.cv}`
+const web = `${data.labelWeb}  ${data.web}`
+const medium = `${data.labelMedium}  ${data.medium}`
+const twitter = `${data.labelTwitter}  ${data.twitter}`
+const github = `${data.labelGitHub}  ${data.github}`
+const card = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+const output = heading +
+  newline + newline +
+  work + newline +
+  location + newline + newline +
+  linkedin + newline +
+  xing + newline +
+  cv + newline +
+  newline +
+  web + newline +
+  medium + newline +
+  twitter + newline +
+  github + newline +
+  newline +
+  card
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
